ddfa232cae2f82365cb4e0eb5c3708a745375bd4
thewbman <thewbman@ddee5aa8-731e-76a5-23d9-a135482a5221>
2011-07-17 07:20:22 +0000

    Release 0.1.1:  - Functioning artists, albums, songs, playlists lists  - Single song playback

---  ampachexl/ampachexl/depends.js
+++  ampachexl/ampachexl/depends.js
@@ -1,6 +1,15 @@
 enyo.depends(
 	"source/ampachexl.js",
 	
+	"source/leftmenu.js",
+	
+	"source/searchselector.js",
+	"source/albumslist.js",
+	"source/playlistslist.js",
+	"source/artistslist.js",
+	"source/videoslist.js",
+	"source/songslist.js",
+	
 	"source/commands.js",
 	"source/sha256.js",
 	"source/XMLObjectifier.js",
---  ampachexl/ampachexl/source/commands.js
+++  ampachexl/ampachexl/source/commands.js
@@ -20,6 +20,74 @@ var defaultCookie = function() {
 	
 };
 
+
+var sort_by = function(field, reverse, primer){
+
+   reverse = (reverse) ? -1 : 1;
+
+   return function(a,b){
+
+       a = a[field];
+       b = b[field];
+
+       if (typeof(primer) != 'undefined'){
+           a = primer(a);
+           b = primer(b);
+       }
+
+       if (a<b) return reverse * -1;
+       if (a>b) return reverse * 1;
+       return 0;
+
+   }
+};
+
+var double_sort_by = function(field1, field2, reverse, primer){
+
+   reverse = (reverse) ? -1 : 1;
+
+   return function(a,b){
+
+       a = a[field1]+"_"+a[field2];
+       b = b[field1]+"_"+b[field2];
+
+       if (typeof(primer) != 'undefined'){
+           a = primer(a);
+           b = primer(b);
+       }
+
+       if (a<b) return reverse * -1;
+       if (a>b) return reverse * 1;
+       
+	   return 0;
+
+   }
+};
+
+var triple_sort_by = function(field1, field2, field3, reverse, primer){
+
+   reverse = (reverse) ? -1 : 1;
+
+   return function(a,b){
+
+       a = a[field1]+"_"+a[field2]+"_"+a[field3];
+       b = b[field1]+"_"+b[field2]+"_"+b[field3];
+
+       if (typeof(primer) != 'undefined'){
+           a = primer(a);
+           b = primer(b);
+       }
+
+       if (a<b) return reverse * -1;
+       if (a>b) return reverse * 1;
+       
+	   return 0;
+
+   }
+};
+
+
+
 var getAmpacheConnectionUrl = function(accountObject) {
 
 	var time = getAmpacheTime();
@@ -37,6 +105,7 @@ var getAmpacheConnectionUrl = function(accountObject) {
 	return connectionUrl;
 	
 };
+
 var getAmpacheTime = function() {
 
 	var myDate = new Date();				// Generic JS date object 
