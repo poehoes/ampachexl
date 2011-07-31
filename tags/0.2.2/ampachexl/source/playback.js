d645b6a0a8e4eca37848197312b99b40c570410a
thewbman <thewbman@ddee5aa8-731e-76a5-23d9-a135482a5221>
2011-07-31 22:55:52 +0000

    Tag 0.2.2

---
+++ tags/0.2.2/ampachexl/source/playback.js
@@ -0,0 +1,402 @@
+/*
+ *   AmapcheXL - A webOS app for Ampache written in the enyo framework and designed for use on a tablet. 
+ *   http://code.google.com/p/ampachexl/
+ *   Copyright (C) 2011  Wes Brown
+ *
+ *   This program is free software; you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation; either version 2 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License along
+ *   with this program; if not, write to the Free Software Foundation, Inc.,
+ *   51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+
+enyo.kind({
+	name: "Playback",
+	className: "Playback",
+	kind: "Control",
+	
+	events: {
+		onViewSelected: "",
+		onDataRequest: "",
+		onUpdateSpinner: "",
+		onPreviousTrack: "",
+		onNextTrack: "",
+		onBannerMessage: "",
+		onUpdatePlaybackStatus: "",
+	},
+	
+	movingSlider: false,
+	startPlayingTimer: "",
+	
+	components: [
+		
+		//{name: "leftMenuScroller", kind: "Scroller", flex: 1, components: [
+			
+			
+			{name: "songArt", kind: "Image"},
+			
+			{name: "songArtist", content: "&nbsp;", allowHtml: true, className: "playbackSongArtist truncating"},
+			{name: "songTitle", content: "&nbsp;", allowHtml: true, className: "playbackSongTitle truncating"},
+			{name: "songAlbum", content: "&nbsp;", allowHtml: true, className: "playbackSongAlbum truncating"},
+			
+			{name: "songTimeWrapper", kind: "HFlexBox", className: "songTimeWrapper", align: "center", pack: "center", components: [
+				{name: "progressTime", className: "playbackTime"},
+				{name: "songSlider", kind: "Slider", className: "songSlider", showing: false, position: 0, flex: 1, onChanging: "songSliderChanging", onChange: "songSliderChange", animationPosition: false},
+				{name: "totalTime", className: "playbackTime"},
+			]},
+		//]},
+		
+			//{name: "songSoundWrapper", className: "songSoundWrapper", showing: true, kind: "Item", layoutKind: "HFlexLayout", align: "center", pack: "center", components: [
+				//{name: "songVideo", kind: "Video", src: "", showControls: true, flex: 1, width: "280px", height: "25px"},
+				{name: "songSound", kind: "Sound", src: "", showControls: true, flex: 1, width: "280px", height: "25px"},
+			//]},
+			
+		{name: "footer", kind: "Toolbar", components: [
+			{kind: "Spacer"},
+			{name: "previous", caption: "<<", showing: false, allowHtml: true, onclick: "previousClick"},
+			{kind: "Spacer"},
+			{name: "play", caption: "Play", showing: false, onclick: "playClick"},
+			{name: "pause", caption: "Pause", showing: false, onclick: "pauseClick"},
+			{kind: "Spacer"},
+			{name: "next", caption: ">>", showing: false, allowHtml: true, onclick: "nextClick"},
+			{kind: "Spacer"},
+		]},
+	],
+	
+	create: function() {
+		if(debug) this.log("create");
+		this.inherited(arguments);
+		
+		//this.render();
+		
+		AmpacheXL.currentAudioObjectIndex = 0;
+		AmpacheXL.nextAudioObjectIndex = 1;
+		AmpacheXL.audioObjects = [];
+		
+		AmpacheXL.audioObjects[AmpacheXL.currentAudioObjectIndex] = new Audio();
+		AmpacheXL.audioObjects[AmpacheXL.nextAudioObjectIndex] = new Audio();
+		
+		AmpacheXL.audioObjects[AmpacheXL.currentAudioObjectIndex].addEventListener("playing", enyo.bind(this, "playingEvent"), false);
+		AmpacheXL.audioObjects[AmpacheXL.currentAudioObjectIndex].addEventListener("pause", enyo.bind(this, "pauseEvent"), false);
+		AmpacheXL.audioObjects[AmpacheXL.currentAudioObjectIndex].addEventListener("timeupdate", enyo.bind(this, "timeupdateEvent"), false);
+		AmpacheXL.audioObjects[AmpacheXL.currentAudioObjectIndex].addEventListener("ended", enyo.bind(this, "endedEvent"), false);
+		AmpacheXL.audioObjects[AmpacheXL.currentAudioObjectIndex].addEventListener("error", enyo.bind(this, "errorEvent"), false);
+		//AmpacheXL.audioObjects[AmpacheXL.currentAudioObjectIndex].addEventListener("progress", enyo.bind(this, "progressEvent"), false);
+		
+		//AmpacheXL.audioObjects[AmpacheXL.nextAudioObjectIndex].addEventListener("progress", enyo.bind(this, "queueProgressEvent"), false);
+		
+	},
+	
+	playSong: function(inSongObject) {
+		if(debug) this.log("playSong: "+enyo.json.stringify(inSongObject));
+		
+		clearTimeout(this.startPlayingTimer);
+		
+		AmpacheXL.currentSong = inSongObject;
+		
+		this.$.songSlider.show();
+		this.$.previous.show();
+		this.$.play.hide();
+		this.$.pause.show();
+		this.$.next.show();
+		
+		this.$.songArt.addClass("playbackSongArt");
+		this.$.songArt.setSrc(inSongObject.art);
+		
+		this.$.songArtist.setContent(inSongObject.artist);
+		this.$.songTitle.setContent(inSongObject.title);
+		this.$.songAlbum.setContent(inSongObject.album);
+		
+		this.render();
+		
+		if((window.PalmSystem)&&(AmpacheXL.prefsCookie.bannerOnPlayback)) this.doBannerMessage(inSongObject.artist+": "+inSongObject.title);
+		
+		if(inSongObject.url == AmpacheXL.nextSong.url) {
+			
+			if(debug) this.log("Already have next song queued, so switch indexes from "+AmpacheXL.currentAudioObjectIndex+" to "+AmpacheXL.nextAudioObjectIndex);
+			
+			AmpacheXL.audioObjects[AmpacheXL.currentAudioObjectIndex].removeEventListener("playing", enyo.bind(this, "playingEvent"), false);
+			AmpacheXL.audioObjects[AmpacheXL.currentAudioObjectIndex].removeEventListener("pause", enyo.bind(this, "pauseEvent"), false);
+			AmpacheXL.audioObjects[AmpacheXL.currentAudioObjectIndex].removeEventListener("timeupdate", enyo.bind(this, "timeupdateEvent"), false);
+			AmpacheXL.audioObjects[AmpacheXL.currentAudioObjectIndex].removeEventListener("ended", enyo.bind(this, "endedEvent"), false);
+			AmpacheXL.audioObjects[AmpacheXL.currentAudioObjectIndex].removeEventListener("error", enyo.bind(this, "errorEvent"), false);
+			//AmpacheXL.audioObjects[AmpacheXL.currentAudioObjectIndex].removeEventListener("progress", enyo.bind(this, "progressEvent"), false);
+			
+			//AmpacheXL.audioObjects[AmpacheXL.nextAudioObjectIndex].removeEventListener("progress", enyo.bind(this, "queueProgressEvent"), false);
+			
+			AmpacheXL.audioObjects[AmpacheXL.currentAudioObjectIndex].pause();
+			AmpacheXL.audioObjects[AmpacheXL.currentAudioObjectIndex].src = "";
+			
+			var newIndex = AmpacheXL.currentAudioObjectIndex
+			AmpacheXL.currentAudioObjectIndex = AmpacheXL.nextAudioObjectIndex;
+			AmpacheXL.nextAudioObjectIndex = newIndex;
+			
+			AmpacheXL.audioObjects[AmpacheXL.currentAudioObjectIndex].addEventListener("playing", enyo.bind(this, "playingEvent"), false);
+			AmpacheXL.audioObjects[AmpacheXL.currentAudioObjectIndex].addEventListener("pause", enyo.bind(this, "pauseEvent"), false);
+			AmpacheXL.audioObjects[AmpacheXL.currentAudioObjectIndex].addEventListener("timeupdate", enyo.bind(this, "timeupdateEvent"), false);
+			AmpacheXL.audioObjects[AmpacheXL.currentAudioObjectIndex].addEventListener("ended", enyo.bind(this, "endedEvent"), false);
+			AmpacheXL.audioObjects[AmpacheXL.currentAudioObjectIndex].addEventListener("error", enyo.bind(this, "errorEvent"), false);
+			//AmpacheXL.audioObjects[AmpacheXL.currentAudioObjectIndex].addEventListener("progress", enyo.bind(this, "progressEvent"), false);
+			
+			//AmpacheXL.audioObjects[AmpacheXL.nextAudioObjectIndex].addEventListener("progress", enyo.bind(this, "queueProgressEvent"), false);
+			
+			if(AmpacheXL.connected) this.$.play.show();
+			this.$.pause.hide();
+			
+			this.startPlayingTimer = setTimeout(enyo.bind(this, "playingFailed"), 8000);
+			AmpacheXL.audioObjects[AmpacheXL.currentAudioObjectIndex].play();
+			
+		} else {
+		
+			if(debug) this.log("Don't have song queued, using existing index at "+AmpacheXL.currentAudioObjectIndex);
+			
+			//remove previous song
+			AmpacheXL.audioObjects[AmpacheXL.currentAudioObjectIndex].pause();
+			AmpacheXL.audioObjects[AmpacheXL.currentAudioObjectIndex].src = "";
+			
+			AmpacheXL.audioObjects[AmpacheXL.currentAudioObjectIndex].src = inSongObject.url;
+			this.startPlayingTimer = setTimeout(enyo.bind(this, "playingFailed"), 8000);
+			AmpacheXL.audioObjects[AmpacheXL.currentAudioObjectIndex].play();
+		
+		}
+		
+	},
+	queueNextSong: function(inSongObject) {
+		if(debug) this.log("queueNextSong for index "+AmpacheXL.nextAudioObjectIndex+": "+enyo.json.stringify(inSongObject));
+		
+		if(inSongObject.url == AmpacheXL.nextSong.url) {
+			if(debug) this.log("we have already queued this song at index: "+AmpacheXL.nextAudioObjectIndex);
+		} else {
+			if(debug) this.log("is a new song - queue it up at index: "+AmpacheXL.audioObjects.length);
+			
+			AmpacheXL.audioObjects[AmpacheXL.nextAudioObjectIndex] = "nothing";
+			
+			AmpacheXL.nextSong = inSongObject;
+			
+			AmpacheXL.nextAudioObjectIndex = AmpacheXL.audioObjects.length;
+			AmpacheXL.audioObjects[AmpacheXL.nextAudioObjectIndex] = new Audio();
+			
+			//AmpacheXL.audioObjects[AmpacheXL.nextAudioObjectIndex].pause();
+			//AmpacheXL.audioObjects[AmpacheXL.nextAudioObjectIndex].src = "";
+			AmpacheXL.audioObjects[AmpacheXL.nextAudioObjectIndex].src = inSongObject.url;
+		}
+		
+		
+	},
+	disconnect: function() {
+		if(debug) this.log("disconnect");
+		
+		AmpacheXL.audioObjects[AmpacheXL.currentAudioObjectIndex].pause();
+		AmpacheXL.audioObjects[AmpacheXL.currentAudioObjectIndex].src = "";
+		
+		AmpacheXL.audioObjects[AmpacheXL.nextAudioObjectIndex].pause();
+		AmpacheXL.audioObjects[AmpacheXL.nextAudioObjectIndex].src = "";
+		
+		this.$.songSlider.hide();
+		this.$.previous.hide();
+		this.$.play.hide();
+		this.$.pause.hide();
+		this.$.next.hide();
+		
+		this.$.songArt.removeClass("playbackSongArt");
+		this.$.songArt.setSrc("");
+		
+		this.$.songTitle.setContent("");
+		this.$.songArtist.setContent("");
+		this.$.songAlbum.setContent("");
+		
+		this.$.progressTime.setContent("");
+		this.$.totalTime.setContent("");
+		
+		//this.playingFailed();
+		
+		this.render();
+	},
+	
+	songSliderChanging: function(inSender, inEvent) {
+		if(debug) this.log("songSliderChanging: "+this.$.songSlider.getPosition());
+		
+		this.movingSlider = true;
+		
+		var newTime = parseInt(this.$.songSlider.getPosition()*AmpacheXL.currentSong.time/100);
+		
+		this.$.progressTime.setContent(floatToTime(newTime));
+		
+	},
+	songSliderChange: function(inSender, inEvent) {
+		if(debug) this.log("songSliderChange: "+this.$.songSlider.getPosition());
+		
+		this.movingSlider = false;
+		
+		var newTime = parseInt(this.$.songSlider.getPosition()*AmpacheXL.currentSong.time/100);
+		
+		AmpacheXL.audioObjects[AmpacheXL.currentAudioObjectIndex].currentTime = newTime;
+		
+	},
+	pauseClick: function() {
+		if(debug) this.log("pauseClick");
+		
+		clearTimeout(this.startPlayingTimer);
+		
+		//this.$.songSound.audio.pause();
+		AmpacheXL.audioObjects[AmpacheXL.currentAudioObjectIndex].pause();
+	},
+	playClick: function() {
+		if(debug) this.log("playClick");
+		
+		clearTimeout(this.startPlayingTimer);
+		
+		//this.$.songSound.audio.play();
+		AmpacheXL.audioObjects[AmpacheXL.currentAudioObjectIndex].play();
+	},
+	previousClick: function() {
+		if(debug) this.log("previousClick");
+		
+		clearTimeout(this.startPlayingTimer);
+		
+		this.doPreviousTrack();
+	},
+	nextClick: function() {
+		if(debug) this.log("nextClick");
+		
+		clearTimeout(this.startPlayingTimer);
+		
+		this.doNextTrack();
+	},
+	
+	playingEvent: function() {
+		if(debug) this.log("playingEvent");
+		
+		clearTimeout(this.startPlayingTimer);
+		
+		this.$.play.hide();
+		if(AmpacheXL.connected) this.$.pause.show();
+		
+		AmpacheXL.currentSong.status = "playing";
+		setTimeout(enyo.bind(this, "doUpdatePlaybackStatus", 10));
+		
+		//this.$.totalTime.setContent(floatToTime(AmpacheXL.audioObjects[AmpacheXL.currentAudioObjectIndex].duration));
+		this.$.totalTime.setContent(floatToTime(AmpacheXL.currentSong.time));
+	},
+	timeupdateEvent: function() {
+		//if(debug) this.log("timeupdateEvent: "+AmpacheXL.audioObjects[AmpacheXL.currentAudioObjectIndex].currentTime);
+		
+		if(AmpacheXL.audioObjects[AmpacheXL.currentAudioObjectIndex].currentTime > 1) clearTimeout(this.startPlayingTimer);
+		
+		//var progress = parseInt(100 * (AmpacheXL.audioObjects[AmpacheXL.currentAudioObjectIndex].currentTime/AmpacheXL.audioObjects[AmpacheXL.currentAudioObjectIndex].duration));
+		var progress = parseInt(100 * (AmpacheXL.audioObjects[AmpacheXL.currentAudioObjectIndex].currentTime/AmpacheXL.currentSong.time));
+		
+		if((!this.movingSlider)&&(AmpacheXL.connected))  {
+			this.$.songSlider.setPosition(progress);
+			this.$.progressTime.setContent(floatToTime(AmpacheXL.audioObjects[AmpacheXL.currentAudioObjectIndex].currentTime));
+		}
+	},
+	pauseEvent: function() {
+		if(debug) this.log("pauseEvent: "+AmpacheXL.audioObjects[AmpacheXL.currentAudioObjectIndex].currentTime);
+		
+		if(AmpacheXL.connected) this.$.play.show();
+		this.$.pause.hide();
+		
+		AmpacheXL.currentSong.status = "paused";
+		setTimeout(enyo.bind(this, "doUpdatePlaybackStatus", 10));
+	},
+	endedEvent: function() {
+		if(debug) this.log("endedEvent");
+		
+		if(AmpacheXL.connected) this.$.play.show();
+		this.$.pause.hide();
+		
+		AmpacheXL.currentSong.status = "stopped";
+		setTimeout(enyo.bind(this, "doUpdatePlaybackStatus", 10));
+		
+		this.doNextTrack();
+	},
+	errorEvent: function() {
+		if(debug) this.log("errorEvent");
+		
+		clearTimeout(this.startPlayingTimer);
+		
+		if(AmpacheXL.audioObjects[AmpacheXL.currentAudioObjectIndex].error) {
+			this.error(AmpacheXL.audioObjects[AmpacheXL.currentAudioObjectIndex].error);
+		
+			this.playingFailed();
+			
+		}
+		
+		//if(AmpacheXL.connected) this.doBannerMessage("Error playing file '"+AmpacheXL.currentSong.title+"'");
+		//this.doNextTrack();
+	},
+	progressEvent: function() {
+		//if(debug) this.log("progressEvent");
+		
+		var endBuf = AmpacheXL.audioObjects[AmpacheXL.currentAudioObjectIndex].buffered.end(0);
+		var soFar = parseInt(((endBuf / AmpacheXL.audioObjects[AmpacheXL.currentAudioObjectIndex].duration) * 100));
+		
+		if(debug) this.log("progressEvent endBuf: "+endBuf+" and soFar:"+soFar+"%");
+	},
+	
+	queueProgressEvent: function() {
+		//if(debug) this.log("queueProgressEvent");
+		
+		var endBuf = AmpacheXL.audioObjects[AmpacheXL.nextAudioObjectIndex].buffered.end(0);
+		var soFar = parseInt(((endBuf / AmpacheXL.audioObjects[AmpacheXL.nextAudioObjectIndex].duration) * 100));
+		
+		if(debug) this.log("queueProgressEvent endBuf: "+endBuf+" and soFar:"+soFar+"%");
+	},
+	
+	playingFailed: function() {
+		if(debug) this.log("playingFailed at currentAudioObjectIndex: "+AmpacheXL.currentAudioObjectIndex);
+		
+		clearTimeout(this.startPlayingTimer);
+		
+		AmpacheXL.audioObjects[AmpacheXL.currentAudioObjectIndex].pause();
+		AmpacheXL.audioObjects[AmpacheXL.currentAudioObjectIndex].src = "";
+			
+		if((AmpacheXL.connected)&&(AmpacheXL.currentSong.url)) {
+		
+			this.doBannerMessage("Error playing file '"+AmpacheXL.currentSong.title+"'");
+			AmpacheXL.currentSong.title = "ERROR: *** "+AmpacheXL.currentSong.title+" ***";
+			AmpacheXL.currentSong.url = null;
+			//AmpacheXL.audioObjects[AmpacheXL.currentAudioObjectIndex] = "nothing";
+			
+			AmpacheXL.audioObjects[AmpacheXL.currentAudioObjectIndex].removeEventListener("playing", enyo.bind(this, "playingEvent"), false);
+			AmpacheXL.audioObjects[AmpacheXL.currentAudioObjectIndex].removeEventListener("pause", enyo.bind(this, "pauseEvent"), false);
+			AmpacheXL.audioObjects[AmpacheXL.currentAudioObjectIndex].removeEventListener("timeupdate", enyo.bind(this, "timeupdateEvent"), false);
+			AmpacheXL.audioObjects[AmpacheXL.currentAudioObjectIndex].removeEventListener("ended", enyo.bind(this, "endedEvent"), false);
+			AmpacheXL.audioObjects[AmpacheXL.currentAudioObjectIndex].removeEventListener("error", enyo.bind(this, "errorEvent"), false);
+			//AmpacheXL.audioObjects[AmpacheXL.currentAudioObjectIndex].removeEventListener("progress", enyo.bind(this, "progressEvent"), false);
+				
+			//AmpacheXL.audioObjects[AmpacheXL.nextAudioObjectIndex].removeEventListener("progress", enyo.bind(this, "queueProgressEvent"), false);
+			
+			AmpacheXL.currentAudioObjectIndex = AmpacheXL.audioObjects.length;
+			AmpacheXL.audioObjects[AmpacheXL.currentAudioObjectIndex] = new Audio();
+			
+			AmpacheXL.audioObjects[AmpacheXL.currentAudioObjectIndex].addEventListener("playing", enyo.bind(this, "playingEvent"), false);
+			AmpacheXL.audioObjects[AmpacheXL.currentAudioObjectIndex].addEventListener("pause", enyo.bind(this, "pauseEvent"), false);
+			AmpacheXL.audioObjects[AmpacheXL.currentAudioObjectIndex].addEventListener("timeupdate", enyo.bind(this, "timeupdateEvent"), false);
+			AmpacheXL.audioObjects[AmpacheXL.currentAudioObjectIndex].addEventListener("ended", enyo.bind(this, "endedEvent"), false);
+			AmpacheXL.audioObjects[AmpacheXL.currentAudioObjectIndex].addEventListener("error", enyo.bind(this, "errorEvent"), false);
+			//AmpacheXL.audioObjects[AmpacheXL.currentAudioObjectIndex].addEventListener("progress", enyo.bind(this, "progressEvent"), false);
+				
+			//AmpacheXL.audioObjects[AmpacheXL.nextAudioObjectIndex].addEventListener("progress", enyo.bind(this, "queueProgressEvent"), false);
+			
+			if(AmpacheXL.connected) this.$.play.show();
+			this.$.pause.hide();
+			
+			setTimeout(enyo.bind(this, "doNextTrack"),1000);
+		}
+	},
+
+});
+	
+
+//asdf
---
+++ tags/0.2.2/ampachexl/source/playlistslist.js
@@ -0,0 +1,306 @@
+/*
+ *   AmapcheXL - A webOS app for Ampache written in the enyo framework and designed for use on a tablet. 
+ *   http://code.google.com/p/ampachexl/
+ *   Copyright (C) 2011  Wes Brown
+ *
+ *   This program is free software; you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation; either version 2 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License along
+ *   with this program; if not, write to the Free Software Foundation, Inc.,
+ *   51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+
+enyo.kind({
+	name: "PlaylistsList",
+	kind: "VFlexBox",
+	className: "PlaylistsList listContent",
+	
+	events: {
+		onViewSelected: "",
+		onDataRequest: "",
+		onUpdateSpinner: "",
+		onOpenWeb: "",
+		onBannerMessage: "",
+		onPreviousView: "",
+	},
+	
+	fullResultsList: [],
+	resultsList: [],
+	
+	components: [
+		{name: "header", kind: "Toolbar", layoutKind: "VFlexLayout", onclick: "headerClick", components: [
+			{name: "headerTitle", kind: "Control", content: "Playlists", className: "headerTitle"},
+			{name: "headerSubtitle", kind: "Control", className: "headerSubtitle"},
+		]},
+		
+		{name: "playlistsSearchInputWrapper", className: "searchInputWrapper", kind: "Item", layoutKind: "HFlexLayout", components: [
+			{name: "playlistsSearchInput", kind: "Input", hint: "Filter", autoCapitalize: "lowercase", oninput: "playlistsInput", flex: 1, components: [
+				{name: "playlistsSearchClear", kind: "Image", src: "images/11-x@2x.png", showing: false, className: "searchClear", onclick: "resetPlaylistsSearch"},
+				{name: "playlistsSearchSpinner", kind: "Spinner"},
+			]}
+		]},
+							
+		{name: "playlistsVirtualList", kind: "VirtualList", onSetupRow: "setupPlaylistsItem", flex: 1, components: [
+			//name: "playlistsDivider", kind: "Divider"},
+			{name: "playlistsItem", kind: "Item", className: "listItem", layoutKind: "HFlexLayout", align: "center", onclick: "playlistsClick", components: [
+				{kind: "VFlexBox", flex: 1, components: [
+					{name: "playlistsTitle", className: "title"},
+					{name: "playlistsSongCount", className: "subtitle"},
+				]},
+			]},
+		]},
+		
+		{name: "footer", kind: "Toolbar", layoutKind: "HFlexLayout", components: [
+			{name: "backCommandIcon", kind: "Control", className: "backCommandIcon", onclick: "doPreviousView"},
+			{kind: "Spacer"},
+			{name: "refreshCommandButton", icon: "images/menu-icon-refresh.png", onclick: "getPlaylists"},
+			{kind: "Spacer"},
+			{name: "backCommandIconSpacer", kind: "Control", className: "backCommandIconSpacer"},
+		]},
+	],
+	
+	create: function() {
+		if(debug) this.log("create");
+		this.inherited(arguments);
+	},
+	
+	activate: function() {
+		if(debug) this.log("activate");
+		
+		this.resize();
+		
+		if(this.fullResultsList.length == 0) {
+			//this.getPlaylists();
+		}
+		
+	},
+	resize: function() {
+		if(debug) this.log("resize");
+		
+		this.$.playlistsVirtualList.resized();
+	},
+	dataRequestResponse: function(inResponse) {
+		if(debug) this.log("dataRequestResponse");
+		
+		/*
+		<playlist id="1234">
+			<name>The Good Stuff</name>
+			<owner>Karl Vollmer</owner>
+			<items>50</items>
+			<tag id="2481" count="2">Rock & Roll</tag>
+			<tag id="2482" count="2">Rock</tag>
+			<tag id="2483" count="1">Roll</tag>
+			<type>Public</type>
+		</playlist>
+		*/
+		
+		this.fullResultsList.length = 0;
+		
+		var xmlobject = (new DOMParser()).parseFromString(inResponse, "text/xml");
+		
+		var errorNodes, singleErrorNode;
+		errorNodes = xmlobject.getElementsByTagName("error");
+		for(var i = 0; i < errorNodes.length; i++) {
+			singleErrorNode = errorNodes[i];
+			
+			this.doBannerMessage("Error: "+singleErrorNode.childNodes[0].nodeValue, true);
+			
+		}
+		
+		var playlistsNodes, singlePlaylistNode, singlePlaylistChildNode;
+		var s = {};
+		
+		playlistsNodes = xmlobject.getElementsByTagName("playlist");
+		for(var i = 0; i < playlistsNodes.length; i++) {
+			singlePlaylistNode = playlistsNodes[i];
+			s = {};
+			
+			s.id = singlePlaylistNode.getAttributeNode("id").nodeValue;
+			
+			for(var j = 0; j < singlePlaylistNode.childNodes.length; j++) {
+				singlePlaylistChildNode = singlePlaylistNode.childNodes[j];
+				
+				switch(singlePlaylistChildNode.nodeName) {
+					case "name":
+						s.name = singlePlaylistChildNode.childNodes[0].nodeValue;
+						break;
+					case "owner":
+						s.owner = singlePlaylistChildNode.childNodes[0].nodeValue;
+						break;
+					case "items":
+						s.items = singlePlaylistChildNode.childNodes[0].nodeValue;
+						break;
+					case "type":
+						s.type = singlePlaylistChildNode.childNodes[0].nodeValue;
+						break;
+				}
+				
+			}
+		
+			s.type = "playlist";
+			
+			this.fullResultsList.push(s);
+		
+		}
+		
+		this.fullResultsList.sort(sort_by("name", false));
+		
+		if(this.fullResultsList.length == AmpacheXL.connectResponse.playlists) {
+			AmpacheXL.allPlaylists = this.fullResultsList.concat([]);
+		}
+		
+		//if(debug) this.log("fullResultsList: "+enyo.json.stringify(this.fullResultsList));
+		
+		this.resetPlaylistsSearch();
+		
+	},
+	
+	getPlaylists: function() {
+		if(debug) this.log("getPlaylists");
+		
+		this.doUpdateSpinner(true);
+		this.doDataRequest("playlistsList", "playlists", "");
+	},
+	resetPlaylistsSearch: function() {
+		if(debug) this.log("resetPlaylistsSearch");
+		
+		this.$.playlistsSearchInput.setValue("");
+		this.$.playlistsSearchClear.hide();
+		this.$.playlistsSearchSpinner.hide();
+		
+		this.finishedGettingPlaylists();
+	
+	},
+	finishedGettingPlaylists: function() {
+		if(debug) this.log("finishedGettingPlaylists");
+		
+		this.resultsList.length = 0;
+		this.resultsList = this.filterPlaylists(this.fullResultsList);
+		
+		this.$.headerSubtitle.setContent(this.resultsList.length+" playlists");
+		
+		this.$.playlistsVirtualList.punt();
+		
+		this.doUpdateSpinner(false);
+		
+	},
+	filterPlaylists: function(inList) {
+		if(debug) this.log("filterPlaylists with list of length: "+inList.length);
+		
+		var finalList = [];
+		var s = {};
+		var filterString = this.$.playlistsSearchInput.getValue().toUpperCase();
+		
+		for(var i = 0; i < inList.length; i++) {
+			s = inList[i];
+		
+			if(s.name.toUpperCase().indexOf(filterString) >= 0) {
+				finalList.push(s);
+			} 
+		}	
+		
+		return finalList;
+	},
+	setupPlaylistsItem: function(inSender, inIndex) {
+		//if(debug) this.log("setupPlaylistsItem: "+inIndex);
+		
+		var row = this.resultsList[inIndex];
+		
+		if(row) {
+		
+			//this.setupPlaylistsDivider(inIndex);
+			//this.$.playlistsItem.applyStyle("border-top", "1px solid silver;");
+			//this.$.playlistsItem.applyStyle("border-bottom", "none;");
+			
+			this.$.playlistsTitle.setContent(row.name);
+			
+			if(row.items == 1) {
+				this.$.playlistsSongCount.setContent("1 items");
+			} else {
+				this.$.playlistsSongCount.setContent(row.items+" items");
+			}
+			
+			
+			return true;
+		
+		}
+	},
+	setupPlaylistsDivider: function(inIndex) {
+		
+		// use group divider at group transition, otherwise use item border for divider
+		var group = this.getPlaylistsGroupName(inIndex);
+		this.$.playlistsDivider.setCaption(group);
+		this.$.playlistsDivider.canGenerate = Boolean(group);
+		if(Boolean(group)) this.$.playlistsItem.applyStyle("border-top", "none");
+		//this.$.playlistsItem.applyStyle("border-bottom", "none;");
+    },
+	getPlaylistsGroupName: function(inIndex) {
+		//if(debug) this.log("getPlaylistsGroupName at index: "+inIndex);
+		
+		var r0 = this.resultsList[inIndex-1];
+		var r1 = this.resultsList[inIndex];
+		
+		var a = r0 && r0.name.substring(0,1);
+		var b = r1.name.substring(0,1);
+		
+		if(!enyo.g11n.Char.isLetter(a)) a = "#";
+		if(!enyo.g11n.Char.isLetter(b)) b = "#";
+		
+		if(inIndex == 0) {
+			return b;
+		} else {
+			return a != b ? b : null;
+		}
+	},
+	
+	headerClick: function() {
+		if(debug) this.log("headerClick");
+		
+		this.$.playlistsVirtualList.punt();
+	},
+	playlistsInput: function() {
+		if(debug) this.log("playlistsInput: "+this.$.playlistsSearchInput.getValue());
+		
+		this.$.playlistsSearchClear.hide();
+		this.$.playlistsSearchSpinner.show();
+		
+		enyo.job("playlistsSearch", enyo.bind(this, "playlistsSearch"),200);
+	},
+	playlistsSearch: function(inSender) {
+		if(debug) this.log("playlistsSearch: "+this.$.playlistsSearchInput.getValue());
+		
+		this.$.playlistsSearchClear.show();
+		this.$.playlistsSearchSpinner.hide();
+		
+		this.finishedGettingPlaylists();
+		
+		//this.$.playlistsSearchInputWrapper.show();
+		this.$.playlistsVirtualList.resized();
+	},
+	playlistsClick: function(inSender, inEvent) {
+		if(debug) this.log("playlistsClick: "+inEvent.rowIndex);
+		
+		var row = this.resultsList[inEvent.rowIndex];
+		
+		AmpacheXL.selectedPlaylist = row;
+		AmpacheXL.selectedAlbum = row;
+		
+		if(debug) this.log("playlistsClick: "+enyo.json.stringify(row));
+		
+		this.doUpdateSpinner(true);
+		this.doDataRequest("songsList", "playlist_songs", "&filter="+row.id);
+		this.doViewSelected("songsList");
+				
+	},
+	
+});
+
